2f59926e725b9b1cc6f62eb1eb00f5a3fb3e8d85
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-03 22:41:58 +0400

    commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let btnSoup = document.getElementById("btnSoup");
+let btnDishes = document.getElementById("btnDishes");
+let btnLunch = document.getElementById("btnLunch");
+let btnDrink = document.getElementById("btnDrink");
+
+btnSoup.addEventListener("click", function () {
+    window.location.href = "soups.html";
+});
---
+++ js/tgscript.js
@@ -0,0 +1,7 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.setHeaderColor("--tg-theme-header-bg-color");
+tg.setBackgroundColor("--tg-theme-bg-color");
+tg.lockOrientation();
