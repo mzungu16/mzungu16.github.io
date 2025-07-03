baec83c09a185386903b7558748da6a00696d52e
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-03 23:38:31 +0400

    commit

--- js/script.js
+++ js/script.js
@@ -10,7 +10,3 @@ let btnSoup = document.getElementById("btnSoup");
 let btnDishes = document.getElementById("btnDishes");
 let btnLunch = document.getElementById("btnLunch");
 let btnDrink = document.getElementById("btnDrink");
-
-btnSoup.addEventListener("click", function () {
-    window.location.href = "soups.html";
-});
