20b34138fa4d2ce892b1bb9e75d48ad7554dba2d
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-04 14:30:55 +0400

    commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-
-tg.setHeaderColor("--tg-theme-header-bg-color");
-tg.setBackgroundColor("--tg-theme-bg-color");
 tg.lockOrientation();
 
 let btnSoup = document.getElementById("btnSoup");
 let btnDishes = document.getElementById("btnDishes");
 let btnLunch = document.getElementById("btnLunch");
 let btnDrink = document.getElementById("btnDrink");
+
+let categoryContainer = document.getElementById("category_id");
+let orderContainer = document.getElementById("order_id");
+orderContainer.style.display = 'none';
+
+btnSoup.addEventListener("click", () => {
+  console.log("Click");
+  categoryContainer.style.display = 'none';
+  tg.BackButton.show();
+  orderContainer.style.display = 'flex';
+});
+
+Telegram.WebApp.onEvent('backButtonClicked', () => {
+  categoryContainer.style.display = 'flex';
+  tg.BackButton.hide();
+  orderContainer.style.display = 'none';
+});
