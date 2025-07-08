2579ac48121a127f236fa2331a590d2ee8e48ac7
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-08 14:35:15 +0400

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,27 +1,57 @@
 let tg = window.Telegram.WebApp;
+let categoryContainer = document.getElementById("category_id");
+let soupContainer = document.getElementById("food_id");
+let dishesContainer = document.getElementById("dishes_id");
+let buttonList = document.querySelectorAll(".category_btn");
 
-
+defaultScreenSettings();
 tg.lockOrientation();
 
-let btnSoup = document.getElementById("btnSoup");
-let btnDishes = document.getElementById("btnDishes");
-let btnLunch = document.getElementById("btnLunch");
-let btnDrink = document.getElementById("btnDrink");
-
-let categoryContainer = document.getElementById("category_id");
-let soupContainer = document.getElementById("order_id");
-
-soupContainer.style.display = 'none';
-
-btnSoup.addEventListener("click", () => {
-  tg.expand();
-  categoryContainer.style.display = 'none';
-  tg.BackButton.show();
-  soupContainer.style.display = 'grid';
+buttonList.forEach(function (i) {
+  i.addEventListener("click", (e) => {
+    console.log(`id: ${e.target.textContent}`);
+    openDivSection(e.target.textContent.toLowerCase());
+  })
 });
 
-Telegram.WebApp.onEvent('backButtonClicked', () => {
+function defaultScreenSettings() {
   categoryContainer.style.display = 'grid';
-  tg.BackButton.hide();
   soupContainer.style.display = 'none';
+  dishesContainer.style.display = 'none';
+}
+
+function soupsScreenSettings() {
+  tg.expand();
+  tg.BackButton.show();
+  categoryContainer.style.display = 'none';
+  soupContainer.style.display = 'grid';
+  dishesContainer.style.display = 'none';
+}
+
+function dishesScreenSettings() {
+  tg.expand();
+  tg.BackButton.show();
+  categoryContainer.style.display = 'none';
+  soupContainer.style.display = 'none';
+  dishesContainer.style.display = 'grid';
+}
+
+function openDivSection(category_txt) {
+  switch (category_txt) {
+    case "супы":
+      soupsScreenSettings();
+      break;
+    case "вторые блюда":
+      dishesScreenSettings();
+      break;
+    case "закуски":
+      break;
+    case "напитки":
+      break;
+  }
+}
+
+Telegram.WebApp.onEvent('backButtonClicked', () => {
+  tg.BackButton.hide();
+  defaultScreenSettings();
 });
