f7f315652ca541c49a1468f157de76fccb7f14ba
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-08 12:33:08 +0400

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand();
+
 tg.lockOrientation();
 
 let btnSoup = document.getElementById("btnSoup");
@@ -9,18 +9,18 @@ let btnLunch = document.getElementById("btnLunch");
 let btnDrink = document.getElementById("btnDrink");
 
 let categoryContainer = document.getElementById("category_id");
-let orderContainer = document.getElementById("order_id");
+let soupContainer = document.getElementById("order_id");
 orderContainer.style.display = 'none';
 
 btnSoup.addEventListener("click", () => {
-  console.log("Click");
+  tg.expand();
   categoryContainer.style.display = 'none';
   tg.BackButton.show();
-  orderContainer.style.display = 'grid';
+  soupContainer.style.display = 'grid';
 });
 
 Telegram.WebApp.onEvent('backButtonClicked', () => {
   categoryContainer.style.display = 'grid';
   tg.BackButton.hide();
-  orderContainer.style.display = 'none';
+  soupContainer.style.display = 'none';
 });
