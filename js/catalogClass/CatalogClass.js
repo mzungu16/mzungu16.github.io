56bffbf490f5754ce62381479b3c2ed8a5dd11f1
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-28 16:20:42 +0400

    change

--- js/catalogClass/CatalogClass.js
+++ js/catalogClass/CatalogClass.js
@@ -58,6 +58,7 @@ class CatalogClass {
             console.log("P - Catalog > back button clicked");
             this.categoryClass.categoryScreenSetup();
             tg.BackButton.hide();
+            tg.offClick();
         });
     }
 
