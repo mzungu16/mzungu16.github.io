d490220d207d90cb67d175eeeba134fb1dd9fc4b
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-28 12:26:19 +0400

    change

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ console.log(tg.version);
 tg.lockOrientation();
 
 window.dishesList = [];
-console.log(`Launch time ${counter += 1}`);
+console.log(`Launch time`);
 const categoryClass = new CategoryClass();
 categoryClass.categoryScreenSetup();
