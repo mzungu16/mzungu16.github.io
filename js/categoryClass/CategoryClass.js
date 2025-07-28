35736a250f9219f5783b77a6cccee846a6bdcf1e
gleb.s.smirnov <gleb.s.smirnov@gmail.com>
2025-07-28 15:47:43 +0400

    change

--- js/categoryClass/CategoryClass.js
+++ js/categoryClass/CategoryClass.js
@@ -41,7 +41,7 @@ class CategoryClass {
     }
 
     telegramSetup() {
-        console.log("₽ - Category tg setup - ", true);
+        console.log("₽ - telegram setup - ", true);
         if (tg.MainButton.isVisible) {
             tg.MainButton.text = "Просмотреть корзину";
         }
